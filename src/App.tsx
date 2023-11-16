c8bba07e6d322db6544a4bce46200619982899a9
fatiseddigh <seddighfati@gmail.com>
2023-11-16 19:58:13 +0330

    feat:add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import Home from "./pages/home/Home";
 
+// import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
 function App() {
-  return (
-    <>
-      <Home />
-    </>
-  );
+  const Layout = () => {
+    return <div className="main"></div>;
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "users",
+      element: <Users />,
+    },
+    {
+      path: "products",
+      element: <Products />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
