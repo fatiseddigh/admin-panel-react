8abaf98ed004c6f8e56b4da7f83c9479ee4eaa47
fatiseddigh <seddighfati@gmail.com>
2023-11-16 20:15:34 +0330

    feat:add layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,23 @@ import Home from "./pages/home/Home";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Users from "./pages/users/Users";
 import Products from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
 function App() {
   const Layout = () => {
-    return <div className="main"></div>;
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer"></div>
+        </div>
+        <Footer />
+      </div>
+    );
   };
 
   const router = createBrowserRouter([
