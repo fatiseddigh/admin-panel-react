3f9302a70a94e4fad3d93794d88a38918cb91eab
fatiseddigh <seddighfati@gmail.com>
2023-11-17 15:46:51 +0330

    feat:add navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,30 @@
 import "./navbar.scss";
 function Navbar() {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="logo" />
+        <span>adminPanel</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img
+            src="https://fastly.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY"
+            alt=""
+          />
+          <span>fati</span>
+        </div>
+        <img src="/setting.svg" alt="" className="icon" />
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
