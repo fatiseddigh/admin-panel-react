c7b887b4bb558b17cb4c8d017d2df6372cc65ddd
fatiseddigh <seddighfati@gmail.com>
2023-11-18 19:06:21 +0330

    feat:add pie chart

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import BarChartBox from "../../components/barChartBox/BarChartBox";
 import ChartBox from "../../components/chartBox/ChartBox";
+import PieChartBox from "../../components/pieChartBox/PieChartBox";
 import TopDealBox from "../../components/topDealBox/TopDealBox";
 import {
   barChartBoxRevenue,
@@ -22,7 +23,9 @@ function Home() {
       <div className="box box3">
         <ChartBox {...chartBoxProduct} />
       </div>
-      <div className="box box4">box4</div>
+      <div className="box box4">
+        <PieChartBox />
+      </div>
       <div className="box box5">
         <ChartBox {...chartBoxRevenue} />
       </div>
